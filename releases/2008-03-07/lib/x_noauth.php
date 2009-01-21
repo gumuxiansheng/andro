97ccd32b78d73e131471afc5f3dd87931a15c130
kendowns <kendowns@d2947088-3ffd-11de-917f-4137a9cc17b9>
2009-01-21 13:07:28 +0000

    Delete a bunch of old releases.

--- releases/2008-03-07/lib/x_noauth.php
+++
@@ -1,9 +0,0 @@
-<?php
-class x_noauth extends x_table2 {
-   // This is a placeholder.  If this page is being called,
-   // we are going to html_noauth.php, which is just an empty page.
-   function main() {
-      echo "";
-   }
-}
-?>
