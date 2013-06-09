76a3459d3c4a7c5f714511d740e4c171af3002c7
dorgan@donaldorgan.com <dorgan@donaldorgan.com@d2947088-3ffd-11de-917f-4137a9cc17b9>
2013-06-09 18:11:02 +0000

    Tweaking disqus settings

--- apps/adocs/trunk/application/rebuild.php
+++ apps/adocs/trunk/application/rebuild.php
@@ -395,8 +395,11 @@ class rebuild extends x_table2 {
         echo('<div id="disqus_thread"></div>
             <script type="text/javascript">
                 var disqus_shortname = \'andro\'; // required: replace example with your forum shortname
+                var disqus_title = \'' .$page .'\';
                 (function() {
-                    var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true;
+                    var dsq = document.createElement(\'script\');
+                    dsq.type = \'text/javascript\';
+                    dsq.async = true;
                     dsq.src = \'//\' + disqus_shortname + \'.disqus.com/embed.js\';
                     (document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq);
                 })();
