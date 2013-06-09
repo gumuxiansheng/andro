738b4742de2986b0970b8a22532acceb712040b3
dorgan@donaldorgan.com <dorgan@donaldorgan.com@d2947088-3ffd-11de-917f-4137a9cc17b9>
2013-06-09 17:58:26 +0000

    Replacing comment system with disqus

--- apps/adocs/trunk/application/rebuild.php
+++ apps/adocs/trunk/application/rebuild.php
@@ -53,7 +53,7 @@ class rebuild extends x_table2 {
         $this->writeToc();
         $this->writePages();
         
-        #exit, don't return
+        #exit, don't returnts_ins
         x_EchoFlush("done");
         exit;
     }
@@ -392,11 +392,20 @@ class rebuild extends x_table2 {
             }
 
         echo $this->childTopics($page);
-        if ( $comments ) {
-            echo $this->addComments($page);
-        }
-        echo "</div>";
-        echo "<div style='clear:both'></div>";
+        echo('<div id="disqus_thread"></div>
+            <script type="text/javascript">
+                var disqus_shortname = \'andro\'; // required: replace example with your forum shortname
+                (function() {
+                    var dsq = document.createElement(\'script\'); dsq.type = \'text/javascript\'; dsq.async = true;
+                    dsq.src = \'//\' + disqus_shortname + \'.disqus.com/embed.js\';
+                    (document.getElementsByTagName(\'head\')[0] || document.getElementsByTagName(\'body\')[0]).appendChild(dsq);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+            <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+        ');
+            echo "</div>";
+            echo "<div style='clear:both'></div>";
         $html = ob_get_clean();
         $name = makeFileStem($page).'.html';
         ob_start();
